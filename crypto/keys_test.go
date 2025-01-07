aafd47c566d8e878d566422c4ceecbb435ec7982
Baryyy <fiodarborisevich12@gmail.com>
2025-01-07 17:18:27 +0300

    test

--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -7,6 +7,26 @@ import (
 
 func TestGeneratePrivateKey(t *testing.T) {
 	privKey := GeneratePrivateKey()
-
 	assert.Equal(t, len(privKey.Bytes()), privKeyLen)
+
+	pubKey := privKey.Public()
+	assert.Equal(t, len(pubKey.Bytes()), pubKeyLen)
+}
+
+func TestPrivateKeySign(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	msg := []byte("True")
+
+	sig := privKey.Sign(msg)
+	assert.True(t, sig.Verify(pubKey, msg))
+
+	// Invalid msg
+	assert.False(t, sig.Verify(pubKey, []byte("false")))
+
+	//Invalid pubKey
+	invalidPrivKey := GeneratePrivateKey()
+	invalidPubKey := invalidPrivKey.Public()
+	assert.False(t, sig.Verify(invalidPubKey, msg))
+
 }
